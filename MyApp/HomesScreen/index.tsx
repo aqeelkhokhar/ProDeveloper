8ef8476dd6b344c724adfe0356167f89a6459c93
aqeel ahmad <aqeelakash596@gmail.com>
2024-05-14 15:27:33 +0500

    firestore database implemented with real time reflect too

--- MyApp/HomesScreen/index.tsx
+++ MyApp/HomesScreen/index.tsx
@@ -1,23 +1,60 @@
-import * as React from "react";
-import {
-    SafeAreaView,
-    Text,
-    View,
-} from "react-native";
-import database from '@react-native-firebase/database';
+import React, {useEffect} from "react";
+import {SafeAreaView, Text, View} from "react-native";
+// import  {useEffect} from 'react-native'
+import firestore from "@react-native-firebase/firestore";
 
+function HomeScreen({navigation}: any) {
+    //for firbase real time database
+    // const reference = database().ref('/users');
+    // console.log('reference', reference);
+    //     database()
+    // .ref('/user')
+    // .on('value', snapshot => {
+    //     console.log('User data: ', snapshot.val());
+    // });
 
+    useEffect(() => {
+        // setFirestoreData();
+        // getFirestoreData();
+    }, []);
 
+    function onResult(QuerySnapshot) {
+        console.log("Got Users collection result.", QuerySnapshot);
+    }
 
-function HomeScreen({ navigation }: any) {
-    //for firbase real time database 
-    const reference = database().ref('/users');
-    console.log('reference', reference);
-        database()
-    .ref('/user')
-    .on('value', snapshot => {
-        console.log('User data: ', snapshot.val());
-    });
+    function onError(error) {
+        console.error(error);
+    }
+
+    //real time listner
+    firestore().collection("ProDeveloperBackend").onSnapshot(onResult, onError);
+
+    async function setFirestoreData() {
+        const res = await firestore()
+            .collection("ProDeveloperBackend")
+            .doc("user")
+            .set({
+                name: "Aqeel Ahmad",
+                country: "Pakistan",
+                province: "Punjab",
+            });
+        console.log("Updated result: ", res);
+    }
+    async function getFirestoreData() {
+        const usersCollection = await firestore()
+            .collection("ProDeveloperBackend")
+            .doc("user")
+            .onSnapshot(documentSnapshot => {
+                console.log("User data: ", documentSnapshot.data());
+            });
+        console.log("ProDeveloperBackend", usersCollection?._data);
+        const managerCollection = await firestore()
+            .collection("ProDeveloperBackend")
+            .doc("Manager")
+            .get();
+        console.log("ProDeveloperBackend", managerCollection?._data);
+    }
+
     return (
         <SafeAreaView style={{flex: 1}}>
             <View>
