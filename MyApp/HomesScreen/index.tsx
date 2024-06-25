1d2c81a77faf42475b29ad06f86c5df32a986ab8
aqeel ahmad <aqeelakash596@gmail.com>
2024-06-25 15:48:04 +0500

    time zone conversion for different countries

--- MyApp/HomesScreen/index.tsx
+++ MyApp/HomesScreen/index.tsx
@@ -7,14 +7,31 @@ import {
     View,
 } from "react-native";
 import DeviceInfo from "react-native-device-info";
+import moment from "moment-timezone";
 
 function HomeScreen({navigation}: any) {
+    const convertTimeZone = (date, fromTimeZone, toTimeZone) => {
+        const sourceTime = moment.tz(date, fromTimeZone);
+        const targetTime = sourceTime.clone().tz(toTimeZone);
+        return targetTime.format("YYYY-MM-DD HH:mm:ss");
+    };
 
+    // Example usage
+    const appointment = "2023-02-20T08:00:00";
+    const sourceTimeZone = "Asia/Riyadh";
+    const targetTimeZone = moment.tz.guess();
+
+    const TargetTime = convertTimeZone(
+        appointment,
+        sourceTimeZone,
+        targetTimeZone,
+    );
+    console.log("Time:", TargetTime);
 
     return (
         <SafeAreaView style={{flex: 1}}>
             <View>
-                <Text onPress={() => navigation.navigate('Notification')}>
+                <Text onPress={() => navigation.navigate("Notification")}>
                     Home Screen
                 </Text>
             </View>
